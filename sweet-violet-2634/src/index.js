d6f4d4a7dda960b3457bb5b4ad17aea386e7c531
Kisha <kisha@cloudflare.com>
2024-05-15 21:12:14 -0500

    Added https to myOrigin

--- sweet-violet-2634/src/index.js
+++ sweet-violet-2634/src/index.js
@@ -61,7 +61,7 @@ var homework3 = {
 
 	async fetch(request, env, ctx) {
 		if (request.method === "GET") {
-			const myOrigin = "www.kishacorp.com";
+			const myOrigin = "https://www.kishacorp.com";
 			const throwaway = "https://httpbin.org/get";
 			let finalResponse = "";
 			

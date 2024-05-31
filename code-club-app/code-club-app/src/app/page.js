1c1f814a6c7e294a6365043bd3ea516f03ef7ffc
Kisha <kisha@cloudflare.com>
2024-05-31 11:43:59 -0500

    Adding in HW2

--- code-club-app/code-club-app/src/app/page.js
+++ code-club-app/code-club-app/src/app/page.js
@@ -5,6 +5,25 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        async fetch(request, env, ctx) {
+                kishaChoices = {
+                        1: "Kisha's random choice",
+                        2: "Kisha's second random choice",
+                        3: "Kisha's third random choice",
+                        4: "Kisha's fourth random choice"
+                },
+
+                //max = 4;
+                responseInteger = Math.ceil(Math.random() * 4),
+
+                randomResponse = kishaChoices[responseInteger],
+            
+                return new Response(randomResponse);
+        };
+
+
+
+
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.js</code>
         </p>

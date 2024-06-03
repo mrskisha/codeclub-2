8d3d543941c1646a911c154818c12bf2d207e39b
Kisha <kisha@cloudflare.com>
2024-06-02 21:30:31 -0500

    Fix response

--- code-club-app/code-club-app/src/app/page.js
+++ code-club-app/code-club-app/src/app/page.js
@@ -2,22 +2,22 @@
 export let runtime = 'edge'
 
 export async function GET(request) {
-          let getRequest = (request) => request.method === "GET";
-    
-        let kishaChoices = {
+  let getRequest = (request) => request.method === "GET";
+  let kishaChoices = {
                         1: "Kisha's random choice",
                         2: "Kisha's second random choice",
                         3: "Kisha's third random choice",
                         4: "Kisha's fourth random choice"
         };
-        let max = 4;
-        let randomResponse = (kishaChoices,max) => {
-          let responseInteger = Math.ceil(Math.random() * max);
-          return kishaChoices[responseInteger];
-        };
-                
-      let response = getRequest ? 'Generated Random Number: ${randomResponse(kishaChoices, max)}';
 
-      return new Response(response);
-        }
+  let max = 4;
+  let randomResponse = (kishaChoices, max) => {
+    let responseInteger = Math.ceil(Math.random() * max);
+    return kishaChoices[responseInteger];
+  };
+                
+  let response = getRequest ? 'Generated Random Number: ${randomResponse(kishaChoices, max)}' : "use GET";
+
+  return new Response(response);
+}
 

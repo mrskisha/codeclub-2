ef3d7d2e5bd10b9441a442f0a44311af72509050
Kisha <kisha@cloudflare.com>
2024-06-02 21:34:29 -0500

    Fix spacing in kishaChoices

--- code-club-app/code-club-app/src/app/page.js
+++ code-club-app/code-club-app/src/app/page.js
@@ -4,11 +4,11 @@ export let runtime = 'edge'
 export async function GET(request) {
   let getRequest = (request) => request.method === "GET";
   let kishaChoices = {
-                        1: "Kisha's random choice",
-                        2: "Kisha's second random choice",
-                        3: "Kisha's third random choice",
-                        4: "Kisha's fourth random choice"
-        };
+    1: "Kisha's random choice",
+    2: "Kisha's second random choice",
+    3: "Kisha's third random choice",
+    4: "Kisha's fourth random choice"
+  };
 
   let max = 4;
   let randomResponse = (kishaChoices, max) => {
